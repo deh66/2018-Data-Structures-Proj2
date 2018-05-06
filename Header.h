394689194ace9397f3437441396a97a8fee416f4
Daniel Hollo <dan.hollo@gmail.com>
2018-05-06 01:40:16 -0400

    initial commit

---
+++ Header.h
@@ -0,0 +1,25 @@
+/********************************
+*	Daniel Hollo				*
+*	UANet ID: deh66				*
+*	UA ID: 2980428				*
+*								*
+*	Data Structures - 3460-316	*
+*	Project 2					*
+*********************************/
+
+// SUBMIT FILE NAME: 0428_316p2.h
+
+#ifndef HEADER_H
+#define HEADER_H
+//#ifndef 0428_316P2_H
+//#define 0428_316P2_H
+
+#include <cstdlib>
+
+
+
+
+
+#endif
+
+#pragma once
---
+++ Source.cpp
@@ -0,0 +1,26 @@
+/********************************
+*	Daniel Hollo				* 
+*	UANet ID: deh66				*
+*	UA ID: 2980428				*
+*								*
+*	Data Structures - 3460-316	*
+*	Project 2					*
+*********************************/
+
+// SUBMIT FILE NAME: 0428_316p2.cpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+
+//#include "0428_316p2.h"
+#include "Header.h"
+
+// REMOVE
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
